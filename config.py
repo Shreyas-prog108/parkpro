72c6e46da0523e76e123dcaf96627966cfb780fe
Shreyas Pandey <shreyaspandey.ethical@gmail.com>
2025-09-02 04:43:27 +0530

    update

--- config.py
+++ config.py
@@ -11,6 +11,7 @@ def get_database_uri():
     db_url = os.getenv("DATABASE_URL")
     if db_url and db_url.startswith("postgres://"):
         db_url = db_url.replace("postgres://", "postgresql://", 1)
+    return db_url or f"sqlite:///parking_details.db"
     # return db_url or f"sqlite:///{os.path.join(INSTANCE_DIR, 'parking_details.db')}"
 
 class Config:

9b774b8793c6e33c4f166319198c7b0bed0d6fe1
Shreyas Pandey <shreyaspandey.ethical@gmail.com>
2025-10-11 00:24:20 +0530

    fixed db migration script from start.sh as my db has shifted from render

--- start.sh
+++ start.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-echo "Running database migrations..."
-flask db upgrade
-echo "Initializing database tables..."
-python -c "from app import app, db; app.app_context().push(); db.create_all()"
 echo "Starting Gunicorn server..."
 exec gunicorn --bind 0.0.0.0:5000 --workers 1 --timeout 120 app:app
